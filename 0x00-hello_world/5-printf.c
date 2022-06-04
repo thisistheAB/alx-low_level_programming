533fe7593f595f1e1e5f7c16db518e044ce477a5
thisistheAB <renbadyz@gmail.com>
2022-06-04 03:08:34 +0300

    5 - Printf - v2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints text
+ *
+ * Returns 0
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return (0);
+}
