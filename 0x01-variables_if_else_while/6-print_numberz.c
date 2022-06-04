d271c4de1c472d08dd265085dbfd206b491bf542
thisistheAB <renbadyz@gmail.com>
2022-06-04 23:14:20 +0500

    6 - Print Numberz - v2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main (void){
+
+    int num = 0;
+
+    while (num < 10) {
+        putchar(num+'0');
+        num++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
