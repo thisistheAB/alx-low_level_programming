6db92f41f5f82623d81b5e2afc878484ceb787f6
thisistheAB <renbadyz@gmail.com>
2022-06-04 23:03:06 +0500

    5 - Print Numbers - v2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main (void){
+
+    int num = 0;
+
+    while (num < 10) {
+        printf("%d", num);
+        num++;
+    }
+
+    return (0);
+}
