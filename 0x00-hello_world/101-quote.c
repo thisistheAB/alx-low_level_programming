8395bd3bcce1ef221212d06fb44b2e3688783aeb
thisistheAB <renbadyz@gmail.com>
2022-06-06 14:30:36 +0500

    101 - Quote - v6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main (void){
+int main(void){
 
-    fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 
-    return (1);
+return (1);
 }
