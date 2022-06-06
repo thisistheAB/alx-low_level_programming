398721c9a7cdb1cb51856282484d2a0d8d9a5383
thisistheAB <renbadyz@gmail.com>
2022-06-06 14:12:53 +0500

    101 - Quote - v2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main (void){
+
+    fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 61, 1, stderr);
+
+    return (1);
+}
