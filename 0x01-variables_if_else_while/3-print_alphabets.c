45d384e9824bd5fa97f597f695263813f595b6cb
thisistheAB <renbadyz@gmail.com>
2022-06-06 15:24:59 +0500

    bettyFix - v8

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,31 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - This is the main function, FUCK BETTY!!!!!!!!
+ *
+ * Return: This is bullshit
+ */
 int main (void){
 
-    char * alphabet = "abcdefghijklmnopqrstuvwxyz";
-    char * capsAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char *alphabet = "abcdefghijklmnopqrstuvwxyz";
+char *capsAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    int i = 0;
-    int d = 0;
-    while (i < 26){putchar(alphabet[i]);i++;}
-    while (d < 26){putchar(capsAlphabet[d]);d++;}
-    putchar('\n');
+int i = 0;
+int d = 0;
 
-    return (0);
+while (i < 26)
+{
+putchar(alphabet[i]);
+i++;
+}
+
+while (d < 26){
+putchar(capsAlphabet[d]);
+d++;
+}
+
+putchar('\n');
+
+return (0);
 }
