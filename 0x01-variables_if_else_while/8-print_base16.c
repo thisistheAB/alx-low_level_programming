04a459123f0e8933c1e1356664c886eeb3f5046b
thisistheAB <renbadyz@gmail.com>
2022-06-06 15:44:35 +0500

    bettyFix - v18

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
-int main (void){
+/**
+ * main - This is the main function, FUCK BETTY!!!!!!!!
+ *
+ * Return: This is bullshit
+ */
+int main(void)
+{
 
-    char * alphabet = "0123456789abcdef";
+char *alphabet = "0123456789abcdef";
 
-    int i = 0;
-    while (i < 16){putchar(alphabet[i]);i++;}
-    putchar('\n');
+int i = 0;
 
-    return (0);
+while (i < 16)
+{
+putchar(alphabet[i]);
+i++;
+}
+
+putchar('\n');
+
+return (0);
 }
