cb1e1f3ac22f5dd69143045fbc706e6f248112c9
thisistheAB <renbadyz@gmail.com>
2022-06-06 14:26:51 +0500

    101 - Quote - v5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 /* betty style doc for function main goes there */
 int main (void){
 
-    fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60, 1, stderr);
+    fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 
     return (1);
 }
