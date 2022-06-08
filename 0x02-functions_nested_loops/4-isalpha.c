1b2ceb2159b1870c3a8d0d651c156232f13fe4e2
thisistheAB <renbadyz@gmail.com>
2022-06-08 23:58:38 +0300

    4 - Is Alpha - v1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - This is the main function, FUCK BETTY!!!!!!!!
+ *
+ * Return: This is bullshit
+ */
+int _isalpha(int c)
+{
+
+int checker = isalpha(c);
+
+if (checker == 0)
+return 0;
+else
+return 1;
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _putchar(char);
 void print_alphabet_x10(void);
+int _isalpha(int c);
