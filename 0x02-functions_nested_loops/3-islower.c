5de837d85399584c691b1b2530b7de946ffebc69
thisistheAB <renbadyz@gmail.com>
2022-06-09 00:15:15 +0300

    3 - Is Lower - v1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - This is the main function, FUCK BETTY!!!!!!!!
+ *@c: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
+ *
+ * Return: This is bullshit
+ */
+int _islower(int c)
+{
+
+int checker = islower(c);
+
+if (checker == 0)
+{
+return (0);
+}
+else
+{
+return (1);
+}
+
+}
