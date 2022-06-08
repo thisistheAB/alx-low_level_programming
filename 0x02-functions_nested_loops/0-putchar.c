f033436b4d06a90552fdcbe3140c67162e0200af
thisistheAB <renbadyz@gmail.com>
2022-06-08 05:41:37 +0300

    0 - Putchar - v1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - This is the main function, FUCK BETTY!!!!!!!!
+ *
+ * Return: This is bullshit
+ */
+int main(void)
+{
+
+printf("_putchar");
+
+return (0);
+}
