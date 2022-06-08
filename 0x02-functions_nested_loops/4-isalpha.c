c362898ed78e35e3dc6bdcaa9655c8770424be48
thisistheAB <renbadyz@gmail.com>
2022-06-09 00:09:47 +0300

    4 - Is Alpha - v3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,7 @@
 
 /**
  * _isalpha - This is the main function, FUCK BETTY!!!!!!!!
+ *@c: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!! CURSE YOU BETTY!!!!!!!!!!!!!!!
  *
  * Return: This is bullshit
  */
@@ -13,11 +14,11 @@ int checker = isalpha(c);
 
 if (checker == 0)
 {
-return 0;
+return (0);
 }
 else
 {
-return 1;
+return (1);
 }
 
 }
