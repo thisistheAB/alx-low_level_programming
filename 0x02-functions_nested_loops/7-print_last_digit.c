e860bad73e6791ae466853656a8d1408f2c0f1d0
thisistheAB <renbadyz@gmail.com>
2022-06-09 12:27:17 +0500

    7 - Print Last Digit - v1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - This is the main function, FUCK BETTY!!!!!!!!
+ *@iNumber: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
+ *
+ * Return: This is bullshit
+ */
+int print_last_digit(int iNumber)
+{
+
+int testNumber = 5536;
+
+int lastDigit = iNumber % 10;
+
+return (lastDigit);
+
+}
