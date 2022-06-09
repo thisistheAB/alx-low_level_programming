2569f0a111ddef2116b48ad888bd53d9e9986a10
thisistheAB <renbadyz@gmail.com>
2022-06-09 12:13:47 +0500

    6 - Absolute Value - v1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include "main.h"
+
+/**
+ * print_alphabet - This is the main function, FUCK BETTY!!!!!!!!
+ *@c: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
+ *
+ * Return: This is bullshit
+ */
+int _abs(int iNumber)
+{
+
+double dNumber = (double) iNumber;
+
+double rawResult = fabs(dNumber);
+
+int result = (int) rawResult;
+
+return (result);
+
+}
