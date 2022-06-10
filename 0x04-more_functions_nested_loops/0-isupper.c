dbd603285105e041a59602f46684c6384c692204
thisistheAB <renbadyz@gmail.com>
2022-06-10 19:36:16 +0300

    0 - isupper - v1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - This is the main function, FUCK BETTY!!!!!!!!
+ *@c: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
+ *
+ * Return: This is bullshit
+ */
+int _isupper(int c)
+{
+
+int checker = isupper(c);
+
+if (checker == 0)
+{
+return (0);
+}
+else
+{
+return (1);
+}
+
+}
