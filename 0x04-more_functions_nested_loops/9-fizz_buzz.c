c563b409e3adc8bf4661c4a199fee4bfe0308681
thisistheAB <renbadyz@gmail.com>
2022-06-10 23:57:00 +0300

    9 - Fizz Buzz - v1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+    int counter = 1;
+
+    while (counter <= 100){
+
+        if((counter % 3) == 0 || (counter % 5) == 0){
+            if ((counter % 3) == 0) {
+                printf("Fizz");
+            }
+            if ((counter % 5) == 0) {
+                printf("Buzz");
+            }
+            printf(" ");
+        }else{
+            printf("%d", counter);
+            printf(" ");
+        }
+
+        counter++;
+
+    }
+
+    return (0);
+}
