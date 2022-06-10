8150840067703130d59a2f4b167ba55b93d61b91
thisistheAB <renbadyz@gmail.com>
2022-06-10 19:43:43 +0300

    1 - Is Digit - v1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - This is the main function, FUCK BETTY!!!!!!!!
+ *@c: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
+ *
+ * Return: This is bullshit
+ */
+int _isdigit(int c)
+{
+
+int checker = isdigit(c);
+
+if (checker == 0)
+{
+return (0);
+}
+else
+{
+return (1);
+}
+
+}
