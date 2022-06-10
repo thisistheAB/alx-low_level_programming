ebcb77843c61079e73042bc4cd39e7b2127dbfed
thisistheAB <renbadyz@gmail.com>
2022-06-10 20:23:05 +0300

    2 - Mul - v1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _mul - This is the main function, FUCK BETTY!!!!!!!!
+ *@a: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
+ *@b: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
+ *
+ * Return: This is bullshit
+ */
+int mul(int a, int b)
+{
+
+int multiplicationResult = a * b;
+
+return (multiplicationResult);
+
+}
