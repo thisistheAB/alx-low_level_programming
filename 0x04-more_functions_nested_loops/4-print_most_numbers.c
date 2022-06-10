99055e162b70eccd44c660c92cfb9f051539a53f
thisistheAB <renbadyz@gmail.com>
2022-06-10 20:40:41 +0300

    4 - Print Most Numbers - v1

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - This is the main function, FUCK BETTY!!!!!!!!
+ *
+ * Return: This is bullshit
+ */
+void print_most_numbers(void)
+{
+
+int counter = 0;
+
+while (counter <= 9)
+{
+if (counter != 2 && counter != 4)
+{
+_putchar(counter + '0');
+}
+counter++;
+}
+
+_putchar('\n');
+
+}
