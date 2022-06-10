a8b0b785362a1952e33b2f3b85d066f267c5c466
thisistheAB <renbadyz@gmail.com>
2022-06-10 21:34:03 +0300

    6 - Print Line - v1

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - This is the main function, FUCK BETTY!!!!!!!!
+ * @c: Fuck this shit
+ *
+ * Return: This is bullshit
+ */
+void print_line(int c)
+{
+
+int counter = 0;
+while (counter < c)
+{
+_putchar('_');
+counter++;
+}
+_putchar('\n');
+
+}
