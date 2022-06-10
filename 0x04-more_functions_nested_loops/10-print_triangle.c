e47456d9a7212c69be302d9501733622a05c0fca
thisistheAB <renbadyz@gmail.com>
2022-06-11 01:09:59 +0300

    10 - Print Triangle - v4

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,8 @@ void print_triangle(int size)
 {
 if (size > 0)
 {
-int counter = 0, spaces = size - 1, spacesCounter, hashtagsCounter, hashtags = 1;
+int counter = 0, spaces = size - 1, spacesCounter, hashtagsCounter;
+int hashtags = 1;
 while (counter < size)
 {
 spacesCounter = 0;
