73ebb9bde95fb7aefa7a43f72112a5757d711eeb
thisistheAB <renbadyz@gmail.com>
2022-06-10 20:24:24 +0300

    2 - Mul - v2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
 /**
  * _mul - This is the main function, FUCK BETTY!!!!!!!!
  *@a: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
- *@b: THE ONE THING I TRULY HATE IN THIS WORLD IS BETTY!!!!
+ *@b: This sucks
  *
  * Return: This is bullshit
  */
