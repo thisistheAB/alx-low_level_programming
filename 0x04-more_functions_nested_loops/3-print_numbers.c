0b6b960ede38d2f40121164c6b3a1c5612bac48e
thisistheAB <renbadyz@gmail.com>
2022-06-10 20:33:49 +0300

    3 - Print Numbers - v1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - This is the main function, FUCK BETTY!!!!!!!!
+ *
+ * Return: This is bullshit
+ */
+void print_numbers(void)
+{
+
+int counter = 0;
+
+while (counter <= 9)
+{
+_putchar(counter + '0');
+counter++;
+}
+
+_putchar('\n');
+
+}
