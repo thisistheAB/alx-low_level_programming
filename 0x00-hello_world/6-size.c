ddcbf7a1d5387b411dd2a43b0d86c9d245759fbf
thisistheAB <renbadyz@gmail.com>
2022-06-11 18:57:12 +0300

    Betty Fix 5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,13 +15,14 @@ long long int sLongLongInt;
 float sFloat;
 
 printf
-("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(sChar));
+("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(sChar));
 printf
 ("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(sInt));
 printf
 ("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(sLongInt));
 printf
-("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(sLongLongInt));
+("Size of a long long int: %lu byte(s)\n",
+ (unsigned long)sizeof(sLongLongInt));
 printf
 ("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(sFloat));
 
