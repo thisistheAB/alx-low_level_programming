b6b618ad27d83d08e5b81cefb4588e14897854ae
thisistheAB <renbadyz@gmail.com>
2022-06-11 18:55:50 +0300

    Betty Fix 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,24 +3,27 @@
 /**
  * main - Entry point
  *
- * prints text
- *
- * Returns 0
+ * Return: 0
  */
 int main(void)
 {
 
-    char sChar;
-    int sInt;
-    long int sLongInt;
-    long long int sLongLongInt;
-    float sFloat;
+char sChar;
+int sInt;
+long int sLongInt;
+long long int sLongLongInt;
+float sFloat;
 
-    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(sChar));
-    printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(sInt));
-    printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(sLongInt));
-    printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(sLongLongInt));
-    printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(sFloat));
+printf
+("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(sChar));
+printf
+("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(sInt));
+printf
+("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(sLongInt));
+printf
+("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(sLongLongInt));
+printf
+("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(sFloat));
 
-    return (0);
+return (0);
 }
