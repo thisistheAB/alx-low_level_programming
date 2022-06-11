d5a34cb0f2564b1e7563cd8a581e66fa57b4bcfe
thisistheAB <renbadyz@gmail.com>
2022-06-11 18:52:37 +0300

    Betty Fix 3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,11 @@
 /**
  * main - Entry point
  *
- * prints text
- *
- * Returns 0
+ * Return: 0
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-    return (0);
+return (0);
 }
