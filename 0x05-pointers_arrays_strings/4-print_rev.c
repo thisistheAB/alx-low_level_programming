60a09741288995bfc827b74d59f8a772a7fadf36
thisistheAB <renbadyz@gmail.com>
2022-06-15 14:01:37 +0300

    4 - Print Reverse - v1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Null pointer exception
+ * @s: Understand the secrets of 369 you will have the keys to the universe
+ *
+ * Return: The forgotten Genius
+ */
+
+void print_rev(const char *s)
+{
+
+int inputLength = _strlen(s);
+
+while (inputLength >= 0)
+{
+
+putchar(s[inputLength]);
+inputLength--;
+
+}
+
+}
