517bc8f402b96d0f3e1d10e6df2093bec4544074
thisistheAB <renbadyz@gmail.com>
2022-06-15 08:35:04 +0300

    2 - Strlen - v1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Nothing
+ * @s: Nothing
+ *
+ * Return: Nothing
+ */
+
+int _strlen(const char *s)
+{
+
+int outputLength = (int) strlen(s);
+
+return outputLength;
+
+}
