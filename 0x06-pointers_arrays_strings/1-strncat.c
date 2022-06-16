ed3fff9ea6b36e26b9bcf9b128f8e6c46e09b781
thisistheAB <renbadyz@gmail.com>
2022-06-16 15:28:16 +0500

    1 - strncat - v1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - Sighhh.... I don't even know what to say at this point
+ * @dest: You're forcing your dirty style of code upon us
+ * @src: This is a violation of my human rights
+ * @n: I'll report you to the international human rights commission
+ *
+ * Return: Horrid violations department
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+char *returnValue = strncat(dest, src, n);
+
+return (returnValue);
+
+}
