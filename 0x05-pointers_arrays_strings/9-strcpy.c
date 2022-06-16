a94444bb2644d923dd6832c6f04a5f138e07a1ff
thisistheAB <renbadyz@gmail.com>
2022-06-16 13:09:35 +0500

    7 - Puts Half - v7

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - I'm so fucking hungry rn
+ * @dest: I'm contemplating eating my own hand
+ * @src: Left or Right?
+ *
+ * Return: Yep, I'm certifiably insane.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+char *returnValue = strcpy(dest, src);
+
+return (returnValue);
+
+}
