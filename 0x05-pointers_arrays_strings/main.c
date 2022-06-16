b0b5d72d9c21f0cdf6826be1e627a5a71b8900ce
thisistheAB <renbadyz@gmail.com>
2022-06-16 14:01:38 +0500

    8 - Print Array - v1

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -9,11 +9,13 @@
 
 int main(void)
 {
-    char s1[98];
-    char *ptr;
+    int array[5];
 
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
     return (0);
 }
