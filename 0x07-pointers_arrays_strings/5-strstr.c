0fd2aac925293d7c8dcd757b50b4d8e8ca5cdd4e
thisistheAB <renbadyz@gmail.com>
2022-06-22 13:16:59 +0500

    5 - strstr - v1

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _strstr - String String
+ * @haystack: Bajaj
+ * @needle: Tagrow
+ *
+ * Return: Indian Scout Bobber Sixty
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+return (strstr(haystack, needle));
+
+}
