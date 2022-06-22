f9840d82df402ab80d5b38fb2f2fb4efca59f5bb
thisistheAB <renbadyz@gmail.com>
2022-06-22 12:26:53 +0500

    2 - strchr - v1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/*
+ * _strchr - Amenadiel
+ * @s: Uriel
+ * @c: Ethaniel
+ *
+ * Return: Semaiel
+ */
+
+char *_strchr(char *s, char c)
+{
+
+return (strchr(s, c));
+
+}
