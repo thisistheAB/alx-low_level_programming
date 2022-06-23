a59a7abd5c3066423cdcd1a42906af3842aaf258
thisistheAB <renbadyz@gmail.com>
2022-06-23 15:01:11 +0500

    100 - set string - v1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - The devil's gonna make me a free man
+ * @s: The devil's gonna set me free
+ * @to:
+ */
+
+void set_string(char **s, char *to)
+{
+
+*s = to;
+
+}
