0c2b395a67b0d6ee9c87ced4a3aee635a3f9c33e
thisistheAB <renbadyz@gmail.com>
2022-06-27 23:15:48 +0300

    0 - Puts Recursion - v1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts_recursion - it puts
+ * @s: it doesn't put
+ *
+ * Return: To put or not to put
+ */
+
+void _puts_recursion(char *s)
+{
+
+puts(s);
+_putchar('\n');
+
+}
