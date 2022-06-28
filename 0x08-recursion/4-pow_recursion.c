d9d75cdb351869e0e230b15c352e52febc10415d
thisistheAB <renbadyz@gmail.com>
2022-06-28 21:20:39 +0300

    4 - Pow Recursion - v1

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Pew di PIE
+ * @x: Alla fiera dell'est
+ * @y: Per due soldi
+ *
+ * Return: Mi son comprato un.... non ricordo il resto...
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+if (y == 1)
+{
+return (x);
+}
+
+return (_pow_recursion(x, y-1)*x);
+
+}
