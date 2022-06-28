5c18067c6a9d9e3bdb3d2a599ba452b81b0cfd27
thisistheAB <renbadyz@gmail.com>
2022-06-28 14:37:19 +0300

    2 - Strlen Recursion - v1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - Place the semen
+ * @s
+ *
+ * Return: Nothing at all
+ */
+
+int _strlen_recursion(char *s)
+{
+
+int inputLength = (int) strlen(s);
+
+return (inputLength);
+
+}
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -8,7 +8,10 @@
  */
 int main(void)
 {
-    _print_rev_recursion("\nColton Walker");
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
     return (0);
 
 }
