898d2be4db67717379e6432ebd598fb5486cf1d0
thisistheAB <renbadyz@gmail.com>
2022-07-03 22:49:04 +0500

    0 - What's My Name - v1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - Main stuff
+ * @argc: Wait so we have to do this each time
+ * @argv: Like every single time
+ *
+ * Return: Oh for fuck sake
+ */
+
+int main (int argc, char *argv[])
+{
+
+printf("%s\n", argv[0]);
+
+return (0);
+
+}
