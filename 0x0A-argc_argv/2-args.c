9d4474d2e9a9f7adb3a2922f01263d812c4f7f91
thisistheAB <renbadyz@gmail.com>
2022-07-05 00:25:18 +0300

    2 - Args - v1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - As around the sun the earth knows she's revolving
+ * @argc: As now can't reveal the mysteries of tomorrow
+ * @argv: But in passing will grow older every day
+ *
+ * Return: Did you know that true love asks for nothing
+ */
+
+int main(int argc, char *argv[])
+{
+
+int argumentIndex = 0;
+
+while (argumentIndex < argc)
+{
+
+printf("%s\n", argv[argumentIndex]);
+argumentIndex++;
+
+}
+
+return (0);
+
+}
