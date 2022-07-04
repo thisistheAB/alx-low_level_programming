c2493aecbc1aa81cc3732c975fd65120b8c79f38
thisistheAB <renbadyz@gmail.com>
2022-07-05 00:36:15 +0300

    3 - Mul - v1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Just as all that's born is new
+ * @argc: You know what I say is true
+ * @argv: That I'll be loving you always
+ *
+ * Return: Until the day is night and night is day
+ */
+
+int main(int argc, char *argv[])
+{
+
+if (argc < 3)
+{
+printf("Error\n");
+return(1);
+}
+
+int multiplicando = atoi(argv[1]);
+int multiplicatore = atoi(argv[2]);
+
+int result = multiplicando * multiplicatore;
+
+printf("%d\n", result);
+
+return(0);
+
+}
