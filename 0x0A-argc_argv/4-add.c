9e4dfc1985081bc653f2e37a96349626c65a5f70
thisistheAB <renbadyz@gmail.com>
2022-07-05 20:15:37 +0500

    This is a commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+
+int containsChars(char *iString);
 
 /**
  * main - Deep down in Louisiana close to New Orleans
@@ -13,41 +16,72 @@
 
 int main(int argc, char *argv[])
 {
+
+int argIndex = 1;
+int checkString;
 int currentInt;
-int digitCheck;
-int additionResult = 0;
-int argIndex = 0;
+int argSum = 0;
 
 if (argc > 1)
 {
+
 while (argIndex < argc)
 {
-digitCheck = isdigit(atoi(argv[argIndex]));
-if (digitCheck != 0)
+
+checkString = containsChars(argv[argIndex]);
+
+if (checkString == 0)
 {
 currentInt = atoi(argv[argIndex]);
-if (currentInt >= 0)
+if (currentInt > 0)
 {
-additionResult = additionResult + currentInt;
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-}
-else
-{
-printf("Error\n");
-return (1);
-}
+argSum = argSum + currentInt;
 argIndex++;
 }
-printf("%d\n", additionResult);
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+
+}
+printf("%d\n", argSum);
 }
 else
 {
 printf("0");
 }
 return (0);
+}
+
+int containsChars(char *iString)
+{
+
+int inputLength = sizeof(iString);
+int inputIndex = 0;
+char currentChar;
+
+while (inputIndex < inputLength)
+{
+
+currentChar = iString[inputIndex];
+if(isalpha(currentChar))
+{
+return (1);
+}
+else
+{
+inputIndex++;
+}
+
+}
+
+return (0);
+
 }
