9aac5c81ec5ecef5732ee4737116064f363a8fd5
thisistheAB <renbadyz@gmail.com>
2022-07-05 21:20:42 +0300

    4 - add - v25

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,64 +16,65 @@ int containsChars(const char *iString);
 
 int main(int argc, char *argv[])
 {
+int argIndex = 1;
+int checkString;
+int currentInt;
+int argSum = 0;
 
-    int argIndex = 1;
-    int checkString;
-    int currentInt;
-    int argSum = 0;
-
-    if (argc > 1)
-    {
-
-        while (argIndex < argc)
-        {
-
-            checkString = containsChars(argv[argIndex]);
-            if (checkString == 0)
-            {
-                currentInt = atoi(argv[argIndex]);
-                argSum = argSum + currentInt;
-                argIndex++;
-            }
-            else
-            {
-                printf("Error\n");
-                return (1);
-            }
-
-        }
-
-        printf("%d\n", argSum);
-
-    }
-    else
-    {
-        printf("0\n");
-    }
-    return (0);
+if (argc > 1)
+{
+while (argIndex < argc)
+{
+checkString = containsChars(argv[argIndex]);
+if (checkString == 0)
+{
+currentInt = atoi(argv[argIndex]);
+argSum = argSum + currentInt;
+argIndex++;
 }
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", argSum);
+}
+else
+{
+printf("0\n");
+}
+return (0);
+}
+
+/**
+ * containsChars - Deep down in Louisiana close to New Orleans
+ * @iString: Way back up in the woods among the evergreens
+ *
+ * Return: Where lived a country boy named Johnny B. Goode
+ */
 
 int containsChars(const char *iString)
 {
 
-    unsigned int inputLength = strlen(iString);
-    unsigned int inputIndex = 0;
-    char currentChar;
+unsigned int inputLength = strlen(iString);
+unsigned int inputIndex = 0;
+char currentChar;
 
-    while (inputIndex < inputLength)
-    {
+while (inputIndex < inputLength)
+{
 
-        currentChar = iString[inputIndex];
-        if(isdigit(currentChar))
-        {
-            inputIndex++;
-        }
-        else
-        {
-            return (1);
-        }
-    }
+currentChar = iString[inputIndex];
+if(isdigit(currentChar))
+{
+inputIndex++;
+}
+else
+{
+return (1);
+}
+}
 
-    return (0);
+return (0);
 
 }
