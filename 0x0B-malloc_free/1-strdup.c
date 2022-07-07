c5b0dd6ea0a76dbad291e00cad7d91a53e84ad0f
thisistheAB <renbadyz@gmail.com>
2022-07-07 09:35:47 +0500

    1 - strdup - v1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - This is some shait
+ * @str: Fun environment
+ *
+ * Return: Also hopelessly useless
+ */
+char *_strdup(char *str)
+{
+
+char *result;
+
+result = strdup(str);
+
+return (result);
+
+}
