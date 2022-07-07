5d6af07045c020423b9fe9162dcc597e69ecd8cb
thisistheAB <renbadyz@gmail.com>
2022-07-07 10:10:25 +0500

    2 - Str Concat - v1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - CONCATACITANITE
+ * @s1: In every life we have some trouble
+ * @s2: But when you worry you make it double
+ *
+ * Return: Don't Worry, Be Happy
+ */
+char *str_concat(char *s1, char *s2)
+{
+
+char *result;
+result = malloc(sizeof(s1) + sizeof(s2));
+
+strncat(result, s1, sizeof(s1) + sizeof(s2));
+strncat(result, s2, sizeof(s1) + sizeof(s2));
+
+return (result);
+
+}
