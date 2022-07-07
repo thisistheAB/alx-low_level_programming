ff069d02aaccc3c431e8bdc792e0cb4d71dbc533
thisistheAB <renbadyz@gmail.com>
2022-07-07 10:23:50 +0500

    2 - Str Concat - v4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,10 @@ char *str_concat(char *s1, char *s2)
 {
 
 char *result;
-int size = sizeof(s1) + sizeof(s2);
 result = malloc(sizeof(s1) + sizeof(s2));
 
-strncat(result, s1, size);
-strncat(result, s2, size);
+strcat(result, s1);
+strcat(result, s2);
 
 return (result);
 
