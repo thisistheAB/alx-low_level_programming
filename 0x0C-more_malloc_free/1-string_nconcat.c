c0e649a1b04c941a015b54e11bc99ab2af477790
thisistheAB <renbadyz@gmail.com>
2022-07-13 12:25:49 +0300

    1 - String Nconcat - v1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - And I wanna kiss you make you feel alright
+ * @s1: I'm just so tired to share my nights
+ * @s2: I wanna cry and I wanna love
+ * @n: But all my tears have been used up
+ *
+ * Return: On another love
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+unsigned int resultLength;
+char *resultString;
+unsigned int firstStringLength;
+unsigned int secondStringLength;
+int indexCounter = 0;
+
+if (s1 == NULL)
+        s1 = "";
+
+if (s2 == NULL)
+        s2 = "";
+
+firstStringLength = strlen(s1);
+secondStringLength = strlen(s2);
+
+if (n > secondStringLength)
+        n = secondStringLength;
+
+resultLength = firstStringLength + n;
+resultString = malloc(resultLength);
+
+while (indexCounter < resultLength)
+{
+if (indexCounter < firstStringLength)
+        resultString[indexCounter] = s1[indexCounter];
+else
+        resultString[indexCounter] = s2[indexCounter - firstStringLength];
+
+indexCounter++;
+}
+resultString[indexCounter] = '\0';
+
+return (resultString);
+}
