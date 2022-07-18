4ccf925d7e8adfaff608f16f2499e614f63c692c
thisistheAB <renbadyz@gmail.com>
2022-07-18 13:29:17 +0500

    2 - Main - v1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Such a damn tragedy
+ *
+ * Return: For you to see so much of my ass and you ask for me
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
