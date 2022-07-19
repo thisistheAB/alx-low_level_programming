8e4abaa8ffff4dd8f016935b012bca2f30f8e4bc
thisistheAB <renbadyz@gmail.com>
2022-07-19 23:32:52 +0500

    0 - Print List Int - v2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 /**
@@ -27,8 +27,8 @@ typedef struct listint_s
  */
 typedef struct listp_s
 {
-    void *p;
-    struct listp_s *next;
+void *p;
+struct listp_s *next;
 } listp_t;
 
 size_t print_listint(const listint_t *h);
